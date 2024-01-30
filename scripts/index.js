e68375b170b8d76f2f4565b84f5c42107eb201fe
pablo-rr <pablorr2@gmail.com>
2024-01-30 13:37:52 +0100

    index.html and css
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

---
+++ scripts/index.js
@@ -0,0 +1,8 @@
+console.log("jskjdaksd")
+
+function projectClicked(project) {
+    if(project == "scale-tale") {
+        console.log("jaja")
+        document.getElementById("scale-tale").classList.toggle("game-showing")
+    }
+}
