fb735117f5641337a80818a46487a5b885c57fa2
pablo-rr <pablorr2@gmail.com>
2024-01-31 18:28:44 +0100

    Version 1
    Signed-off-by: pablo-rr <pablorr2@gmail.com>

--- scripts/index.js
+++ scripts/index.js
@@ -1,8 +1,44 @@
-console.log("jskjdaksd")
+let gameSwitching = false;
+let activeGame = 0;
+let projects = ["scale-tale", "nightmare-run", "glass-jungle"];
 
 function projectClicked(project) {
-    if(project == "scale-tale") {
-        console.log("jaja")
-        document.getElementById("scale-tale").classList.toggle("game-showing")
+    for(let i = 0; i < projects.length; i++){
+        if(projects[i] == project) {
+            document.getElementById(projects[activeGame]).classList.toggle("game-showing");
+            document.getElementById(projects[activeGame]).classList.toggle("game-hidden");
+            document.getElementById(project).classList.toggle("game-showing");
+            document.getElementById(project).classList.toggle("game-hidden");
+            activeGame = projects.indexOf(project);
+            return;
+        }
     }
+}
+
+function projectsClicked() {
+    // document.getElementById("main-menu-projects").classList.toggle("main-menu-projects-hidden");
+    // document.getElementById("main-menu-projects").classList.toggle("main-menu-projects-show");
+    document.getElementById("main-menu-projects").classList.toggle("collapsed");
+}
+
+window.onload = () => {
+    addEventListener("wheel", (ev) => {
+        document.getElementById(projects[activeGame]).classList.toggle("game-showing");
+        document.getElementById(projects[activeGame]).classList.toggle("game-hidden");
+
+        if(ev.deltaY > 0){
+            activeGame += 1;
+            if(activeGame >= projects.length){
+                activeGame = 0;
+            }
+        } else {
+            activeGame -= 1;
+            if(activeGame < 0){
+                activeGame = projects.length - 1;
+            }
+        }
+
+        document.getElementById(projects[activeGame]).classList.toggle("game-showing");
+        document.getElementById(projects[activeGame]).classList.toggle("game-hidden");      
+    })
 }
